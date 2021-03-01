74839c1ea4b65a28743c4d4a93756692bfcf4161
Gabriel Orenic <gaorenic@gmail.com>
2021-03-01 10:24:31 -0600

    Initial Commit

---
+++ Map Hackwhich/Map_HackwhichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwhichApp.swift
+//  Map Hackwhich
+//
+//  Created by Student on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackwhichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
